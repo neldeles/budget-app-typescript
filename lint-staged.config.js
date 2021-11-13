f2d20a9798a324c51042221f8a1e9954680a6003
neldeles <neldeles@gmail.com>
2021-11-13 12:13:48 +0800

    testing lint staged 1

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "*.{ts,tsx}": (filenames) => [
+    "yarn format:fix",
+    "yarn test --bail --watchAll=false --findRelatedTests --passWithNoTests",
+    "yarn type:check",
+  ],
+};
