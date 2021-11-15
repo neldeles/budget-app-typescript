10bae16b8e1eb95be88bd9ee8302784d05fa8934
neldeles <neldeles@gmail.com>
2021-11-15 10:42:50 +0800

    add tsc to pre commit checks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "*.{ts,tsx}": [
     "yarn format:fix",
     "yarn lint",
+    "yarn type:check",
     "yarn test --bail --watchAll=false --findRelatedTests --passWithNoTests",
   ],
 };
