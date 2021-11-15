c5d571bd6494d17a8a724f6f4c1c0efc68dec35b
neldeles <neldeles@gmail.com>
2021-11-15 14:18:43 +0800

    fixed lint staged config to fix tsc on only staged files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "*.{ts,tsx}": [
     "yarn format:fix",
     "yarn lint",
+    () => "tsc --noEmit",
     "yarn test --bail --watchAll=false --findRelatedTests --passWithNoTests",
   ],
 };
