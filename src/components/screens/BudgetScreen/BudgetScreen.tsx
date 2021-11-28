819dcada145be3d0ef56cd5141a7b8631c08c22b
neldeles <neldeles@gmail.com>
2021-11-28 13:06:32 +0800

    feat: add category row button
    button to create category row in its category group table

--- src/components/screens/BudgetScreen/BudgetScreen.tsx
+++ src/components/screens/BudgetScreen/BudgetScreen.tsx
@@ -1,4 +1,8 @@
+import { classNames } from "utils/classNames";
+import { PlusIcon } from "@heroicons/react/solid";
+import { Modal } from "components/atoms/Modal";
 import { DashboardContainer } from "components/molecules/DashboardContainer";
+import { useMemo } from "react";
 import { Header } from "components/molecules/Header";
 import { Table } from "components/molecules/Table";
 import { TUser } from "types/global";
@@ -26,24 +30,66 @@ export function BudgetScreen() {
   );
   // TODO: fetch all categories of user
   // TODO: Populate each `category_group` table w its corresponding `category` rows (matching `category_group.id`)
-  const columns = [
-    {
-      Header: "category",
-      accessor: "category",
-    },
-    {
-      Header: "budgeted",
-      accessor: "budgeted",
-    },
-    {
-      Header: "activity",
-      accessor: "activity",
-    },
-    {
-      Header: "available",
-      accessor: "available",
-    },
-  ];
+  // DOING:0 # Ability to add category row entry
+  // - [ ] add cateogry button beside "category" col header
+  // - on click opens modal form
+  //    - [ ] create modal form
+  // - on submit creates a new category row
+  //    - [ ] create category row service handler
+  //    - [ ] msw mock handler
+  //    - [ ] useQuery for category row
+  const columns = useMemo(
+    () => [
+      {
+        Header: () => {
+          return (
+            <div className="inline-flex items-center">
+              <Modal>
+                <span>category</span>
+                <Modal.OpenButton>
+                  <button
+                    className={classNames(
+                      // "inline-flex items-center ml-2 padding[0.1rem]",
+                      "inline-flex items-center ml-2 ",
+                      "text-white bg-gray-400 rounded-full border border-transparent shadow-sm",
+                      "hover:bg-green-400",
+                      "active:bg-green-400 active:ring-2 active:ring-green-500 active:ring-offset-2 active:outline-none",
+                      "focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2 focus-visible:outline-none",
+                      "focus:outline-none"
+                    )}
+                  >
+                    <PlusIcon className="w-4 h-4" aria-hidden="true" />
+                  </button>
+                </Modal.OpenButton>
+                <Modal.Content>
+                  {/* <FormAddCategory
+                      confirmButtonValue="Submit"
+                      categoryGroupId={props.initialState}
+                      title="Create Category"
+                    /> */}
+                  Some content
+                </Modal.Content>
+              </Modal>
+            </div>
+          );
+        },
+        accessor: "category",
+      },
+      {
+        Header: "budgeted",
+        accessor: "budgeted",
+      },
+      {
+        Header: "activity",
+        accessor: "activity",
+      },
+      {
+        Header: "available",
+        accessor: "available",
+      },
+    ],
+    []
+  );
 
   // TODO: because we have set initialData, refetch errors will be treated
   // like background errors. Should setup toast error notifications for this.
