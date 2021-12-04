001399add9a82e52300b5903fc565ee57266e947
neldeles <neldeles@gmail.com>
2021-12-04 22:29:10 +0800

    chore: upgrade to Storybook 6.4
    Try out interactive testing. Try out the linter.

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
   ],
   typescript: {
     check: false,
